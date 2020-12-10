bdbb3e8b5332cdc074122b442977d28370c77040
sofiaelsberg <56071273+sofiaelsberg@users.noreply.github.com>
2020-12-11 00:04:47 +0100

    favicon

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 "use strict";
+
+// $(document).scroll(function () {
+//     var y = $(this).scrollTop();
+//     if (y > 800) {
+//         $('.scroll-animation').fadeIn();
+//     } else {
+//         $('.scroll-animation').fadeOut();
+//     }
+// });
